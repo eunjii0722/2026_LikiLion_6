af11de90d228f1c27e5cc0467dbc42299879e840
EunHye-03 <mgs10204@gmail.com>
2026-05-12 14:49:00 +0900

    feat: google auth credential refresh

---
+++ wize-demo/services/google_auth.py
@@ -0,0 +1,22 @@
+import os
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/forms.responses.readonly",
+]
+
+def get_credentials() -> Credentials:
+    creds = Credentials(
+        token=None,
+        refresh_token=os.getenv("GOOGLE_REFRESH_TOKEN"),
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.getenv("GOOGLE_CLIENT_ID"),
+        client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+        scopes=SCOPES,
+    )
+    creds.refresh(Request())
+    return creds
